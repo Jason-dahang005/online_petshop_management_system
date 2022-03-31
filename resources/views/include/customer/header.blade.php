1688edb1ddfc2bd10b318e82a11405e71a54a986
Jason-dahang005 <jasondahang005@gmail.com>
2022-03-31 10:39:28 +0800

    initial commit

---
+++ resources/views/include/customer/header.blade.php
@@ -0,0 +1,206 @@
+
+    <!-- Start Header Area -->
+    <header class="header navbar-area">
+        <!-- Start Header Middle -->
+        <div class="header-middle">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-3 col-7">
+                        <!-- Start Header Logo -->
+                        <a class="navbar-brand" href="index.html">
+                            <img src="assets/images/logo/logo.svg" alt="Logo">
+                        </a>
+                        <!-- End Header Logo -->
+                    </div>
+                    <div class="col-lg-5 col-md-7 d-xs-none">
+                        <!-- Start Main Menu Search -->
+                        <div class="main-menu-search">
+                            <!-- navbar search start -->
+                            <div class="navbar-search search-style-5">
+                                <div class="search-input">
+                                    <input type="text" placeholder="Search">
+                                </div>
+                                <div class="search-btn">
+                                    <button><i class="lni lni-search-alt"></i></button>
+                                </div>
+                            </div>
+                            <!-- navbar search Ends -->
+                        </div>
+                        <!-- End Main Menu Search -->
+                    </div>
+                    <div class="col-lg-4 col-md-2 col-5">
+                        <div class="middle-right-area">
+                            <div class="nav-hotline">
+                                <i class="lni lni-phone"></i>
+                                <h3>Hotline:
+                                    <span>(+100) 123 456 7890</span>
+                                </h3>
+                            </div>
+                            <div class="navbar-cart">
+                                <div class="wishlist">
+																	<a href="javascript:void(0)">
+																		<i class="lni lni-heart"></i>
+																		<span class="total-items">0</span>
+																	</a>
+                                </div>
+                                <div class="cart-items">
+																	<a href="@guest{{ route('login') }}@endguest" class="main-btn">
+																		<i class="lni lni-cart"></i>
+																		<span class="total-items">2</span>
+																	</a>
+																	@auth
+																		<!-- Shopping Item -->
+																		<div class="shopping-item">
+																			<div class="dropdown-cart-header">
+																				<span>2 Items</span>
+																				<a href="cart.html">View Cart</a>
+																			</div>
+																			<ul class="shopping-list">
+																				<li>
+																					<a href="javascript:void(0)" class="remove" title="Remove this item"><i class="lni lni-close"></i></a>
+																					<div class="cart-img-head">
+																						<a class="cart-img" href="product-details.html"><img src="assets/images/header/cart-items/item1.jpg" alt="#"></a>
+																					</div>
+
+																					<div class="content">
+																						<h4><a href="product-details.html">Apple Watch Series 6</a></h4>
+																						<p class="quantity">1x - <span class="amount">$99.00</span></p>
+																					</div>
+																				</li>
+																				<li>
+																					<a href="javascript:void(0)" class="remove" title="Remove this item"><i class="lni lni-close"></i></a>
+																					<div class="cart-img-head">
+																						<a class="cart-img" href="product-details.html"><img src="assets/images/header/cart-items/item2.jpg" alt="#"></a>
+																					</div>
+																					<div class="content">
+																						<h4><a href="product-details.html">Wi-Fi Smart Camera</a></h4>
+																						<p class="quantity">1x - <span class="amount">$35.00</span></p>
+																					</div>
+																				</li>
+																			</ul>
+																			<div class="bottom">
+																				<div class="total">
+																					<span>Total</span>
+																					<span class="total-amount">$134.00</span>
+																				</div>
+																				<div class="button">
+																					<a href="checkout.html" class="btn animate">Checkout</a>
+																				</div>
+																			</div>
+																		</div>
+																		<!--/ End Shopping Item -->
+																	@endauth
+                                </div>
+                                <div class="auth-user">
+																	<a href="" class="main-btn">
+																		@guest
+																		<i class="lni lni-user"></i>
+																		@endguest
+						
+																		@auth
+																			{{ Auth::user()->name }}
+																		@endauth
+																	</a>
+																	<div class="auth-user-options">
+																		<ul class="option-list">
+																			@guest
+																				<li><a href="{{ route('login') }}">Login</a></li>
+																				<li><a href="{{ route('register') }}">Register</a></li>
+																			@endguest
+						
+																			@auth
+																				<li><a href="">Profile</a></li>
+																				<li><a href="">Orders</a></li>
+																				<li><a href="">Wishlist</a></li>
+																				<li><a href="">Followed Stores</a></li>
+																				{{-- <li><a href="{{ route('logout') }}">Logout</a></li> --}}
+																				<li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a></li>
+																				<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+																					@csrf
+																				</form>
+																			@endauth
+																		</ul>
+																	</div>
+																</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Header Middle -->
+        <!-- Start Header Bottom -->
+        <div class="container">
+					<div class="row align-items-center">
+						<div class="col-lg-8 col-md-6 col-12">
+							<div class="nav-inner">
+								<!-- Start Mega Category Menu -->
+								<div class="mega-category-menu">
+									<span class="cat-button"><i class="lni lni-menu"></i>All Categories</span>
+									<ul class="sub-category">
+										<li><a href="product-grids.html">Electronics <i class="lni lni-chevron-right"></i></a>
+											<ul class="inner-sub-category">
+												<li><a href="product-grids.html">Digital Cameras</a></li>
+												<li><a href="product-grids.html">Camcorders</a></li>
+												<li><a href="product-grids.html">Camera Drones</a></li>
+												<li><a href="product-grids.html">Smart Watches</a></li>
+												<li><a href="product-grids.html">Headphones</a></li>
+												<li><a href="product-grids.html">MP3 Players</a></li>
+												<li><a href="product-grids.html">Microphones</a></li>
+												<li><a href="product-grids.html">Chargers</a></li>
+												<li><a href="product-grids.html">Batteries</a></li>
+												<li><a href="product-grids.html">Cables & Adapters</a></li>
+											</ul>
+										</li>
+										<li><a href="product-grids.html">accessories</a></li>
+										<li><a href="product-grids.html">Televisions</a></li>
+										<li><a href="product-grids.html">best selling</a></li>
+										<li><a href="product-grids.html">top 100 offer</a></li>
+										<li><a href="product-grids.html">sunglass</a></li>
+										<li><a href="product-grids.html">watch</a></li>
+										<li><a href="product-grids.html">man’s product</a></li>
+										<li><a href="product-grids.html">Home Audio & Theater</a></li>
+										<li><a href="product-grids.html">Computers & Tablets </a></li>
+										<li><a href="product-grids.html">Video Games </a></li>
+										<li><a href="product-grids.html">Home Appliances </a></li>
+									</ul>
+								</div>
+								<!-- End Mega Category Menu -->
+								<!-- Start Navbar -->
+								<nav class="navbar navbar-expand-lg">
+									<button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="toggler-icon"></span>
+										<span class="toggler-icon"></span>
+										<span class="toggler-icon"></span>
+									</button>
+									<div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+										<ul id="nav" class="navbar-nav ms-auto">
+											<li class="nav-item"><a href="index.html" class="active" aria-label="Toggle navigation">Home</a></li>
+											<li class="nav-item"><a href="index.html" class="active" aria-label="Toggle navigation">Goldfish</a></li>
+											<li class="nav-item"><a href="index.html" class="active" aria-label="Toggle navigation">Shop</a></li>
+											<li class="nav-item"><a href="index.html" class="active" aria-label="Toggle navigation">About Us</a></li>
+											<li class="nav-item"><a href="contact.html" aria-label="Toggle navigation">Contact Us</a></li>
+										</ul>
+									</div> <!-- navbar collapse -->
+								</nav>
+								<!-- End Navbar -->
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-12">
+								<!-- Start Nav Social -->
+							<div class="nav-social">
+								<h5 class="title">Follow Us:</h5>
+								<ul>
+									<li><a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a></li>
+									<li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a></li>
+									<li><a href="javascript:void(0)"><i class="lni lni-instagram"></i></a></li>
+									<li><a href="javascript:void(0)"><i class="lni lni-skype"></i></a></li>
+								</ul>
+							</div>
+							<!-- End Nav Social -->
+						</div>
+					</div>
+        </div>
+        <!-- End Header Bottom -->
+    </header>
+    <!-- End Header Area -->
---
+++ resources/views/livewire/customer/home.blade.php
@@ -0,0 +1,201 @@
+<div>
+    <!-- Start Hero Area -->
+    <section class="hero-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-12 custom-padding-right">
+                    <div class="slider-head">
+                        <!-- Start Hero Slider -->
+                        <div class="hero-slider">
+                            <!-- Start Single Slider -->
+                            <div class="single-slider"
+                                style="background-image: url(https://via.placeholder.com/800x500);">
+                                <div class="content">
+                                    <h2><span>No restocking fee ($35 savings)</span>
+                                        M75 Sport Watch
+                                    </h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut
+                                        labore dolore magna aliqua.</p>
+                                    <h3><span>Now Only</span> $320.99</h3>
+                                    <div class="button">
+                                        <a href="product-grids.html" class="btn">Shop Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Slider -->
+                            <!-- Start Single Slider -->
+                            <div class="single-slider"
+                                style="background-image: url(https://via.placeholder.com/800x500);">
+                                <div class="content">
+                                    <h2><span>Big Sale Offer</span>
+                                        Get the Best Deal on CCTV Camera
+                                    </h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut
+                                        labore dolore magna aliqua.</p>
+                                    <h3><span>Combo Only:</span> $590.00</h3>
+                                    <div class="button">
+                                        <a href="product-grids.html" class="btn">Shop Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Slider -->
+                        </div>
+                        <!-- End Hero Slider -->
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-6 col-12 md-custom-padding">
+                            <!-- Start Small Banner -->
+                            <div class="hero-small-banner"
+                                style="background-image: url('https://via.placeholder.com/370x250');">
+                                <div class="content">
+                                    <h2>
+                                        <span>New line required</span>
+                                        iPhone 12 Pro Max
+                                    </h2>
+                                    <h3>$259.99</h3>
+                                </div>
+                            </div>
+                            <!-- End Small Banner -->
+                        </div>
+                        <div class="col-lg-12 col-md-6 col-12">
+                            <!-- Start Small Banner -->
+                            <div class="hero-small-banner style2">
+                                <div class="content">
+                                    <h2>Weekly Sale!</h2>
+                                    <p>Saving up to 50% off all online store items this week.</p>
+                                    <div class="button">
+                                        <a class="btn" href="product-grids.html">Shop Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Start Small Banner -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Hero Area -->
+
+    <!-- Start Featured Categories Area -->
+    <section class="featured-categories section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title">
+                        <h2>Featured Categories</h2>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have
+                            suffered alteration in some form.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Single Category -->
+                    <div class="single-category">
+                        <h3 class="heading">TV & Audios</h3>
+                        <ul>
+                            <li><a href="product-grids.html">Smart Television</a></li>
+                            <li><a href="product-grids.html">QLED TV</a></li>
+                            <li><a href="product-grids.html">Audios</a></li>
+                            <li><a href="product-grids.html">Headphones</a></li>
+                            <li><a href="product-grids.html">View All</a></li>
+                        </ul>
+                        <div class="images">
+                            <img src="https://via.placeholder.com/180x180" alt="#">
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Single Category -->
+                    <div class="single-category">
+                        <h3 class="heading">Desktop & Laptop</h3>
+                        <ul>
+                            <li><a href="product-grids.html">Smart Television</a></li>
+                            <li><a href="product-grids.html">QLED TV</a></li>
+                            <li><a href="product-grids.html">Audios</a></li>
+                            <li><a href="product-grids.html">Headphones</a></li>
+                            <li><a href="product-grids.html">View All</a></li>
+                        </ul>
+                        <div class="images">
+                            <img src="https://via.placeholder.com/180x180" alt="#">
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Single Category -->
+                    <div class="single-category">
+                        <h3 class="heading">Cctv Camera</h3>
+                        <ul>
+                            <li><a href="product-grids.html">Smart Television</a></li>
+                            <li><a href="product-grids.html">QLED TV</a></li>
+                            <li><a href="product-grids.html">Audios</a></li>
+                            <li><a href="product-grids.html">Headphones</a></li>
+                            <li><a href="product-grids.html">View All</a></li>
+                        </ul>
+                        <div class="images">
+                            <img src="https://via.placeholder.com/180x180" alt="#">
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Single Category -->
+                    <div class="single-category">
+                        <h3 class="heading">Dslr Camera</h3>
+                        <ul>
+                            <li><a href="product-grids.html">Smart Television</a></li>
+                            <li><a href="product-grids.html">QLED TV</a></li>
+                            <li><a href="product-grids.html">Audios</a></li>
+                            <li><a href="product-grids.html">Headphones</a></li>
+                            <li><a href="product-grids.html">View All</a></li>
+                        </ul>
+                        <div class="images">
+                            <img src="https://via.placeholder.com/180x180" alt="#">
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Single Category -->
+                    <div class="single-category">
+                        <h3 class="heading">Smart Phones</h3>
+                        <ul>
+                            <li><a href="product-grids.html">Smart Television</a></li>
+                            <li><a href="product-grids.html">QLED TV</a></li>
+                            <li><a href="product-grids.html">Audios</a></li>
+                            <li><a href="product-grids.html">Headphones</a></li>
+                            <li><a href="product-grids.html">View All</a></li>
+                        </ul>
+                        <div class="images">
+                            <img src="https://via.placeholder.com/180x180" alt="#">
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Single Category -->
+                    <div class="single-category">
+                        <h3 class="heading">Game Console</h3>
+                        <ul>
+                            <li><a href="product-grids.html">Smart Television</a></li>
+                            <li><a href="product-grids.html">QLED TV</a></li>
+                            <li><a href="product-grids.html">Audios</a></li>
+                            <li><a href="product-grids.html">Headphones</a></li>
+                            <li><a href="product-grids.html">View All</a></li>
+                        </ul>
+                        <div class="images">
+                            <img src="https://via.placeholder.com/180x180" alt="#">
+                        </div>
+                    </div>
+                    <!-- End Single Category -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Features Area -->
+</div>
